52bf889dfed2184e515b1389d9decfb54eef67cc
dilip.malani <mail2dilz@gmail.com>
2026-08-07 11:30:30 +0530

    Changed the Code to Clean Code with Industry Standards Reviewed by SonarQube on 07-08-2026T10:45

---
+++ README.md
@@ -0,0 +1,7 @@
+# Demo
+
+This is a demo project to showcase the 
+capabilities of the code generation model. 
+The project includes various examples and 
+use cases to demonstrate how the model 
+can be utilized effectively.
--- src/main/java/com/capgemini/Main.java
+++ src/main/java/com/capgemini/Main.java
@@ -2,8 +2,6 @@ package com.capgemini;
 
 import java.util.logging.Logger;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static final Logger logger = Logger.getLogger(Main.class.getName());
 
@@ -16,10 +14,8 @@ public class Main {
 
         boolean flag = true;
         while (flag) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-                    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-                    logger.info("Count is: " + getCount());
-                    if (count > 5) flag = false;
+            logger.info("Count is: " + getCount());
+            if (count > 5) flag = false;
         }
     }
 
