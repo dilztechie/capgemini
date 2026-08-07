858de42f314af9f2639a907ea718ae6c6624aacd
dilip.malani <mail2dilz@gmail.com>
2026-08-07 10:55:16 +0530

    Changed the Code to Clean Code with Industry Standards Reviewed by SonarQube on 07-08-2026T10:45

--- src/main/java/com/capgemini/Main.java
+++ src/main/java/com/capgemini/Main.java
@@ -1,17 +1,27 @@
 package com.capgemini;
 
+import java.util.logging.Logger;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    static final Logger logger = Logger.getLogger(Main.class.getName());
+
+    static int count;
+
     static void main() {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+        logger.log(java.util.logging.Level.INFO, "Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
+        boolean flag = true;
+        while (flag) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+                    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+                    logger.info("Count is: " + getCount());
+                    if (count > 5) flag = false;
         }
     }
+
+    static int getCount() { return count++; }
 }
